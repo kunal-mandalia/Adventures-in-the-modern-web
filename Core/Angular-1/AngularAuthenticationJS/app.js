d51c5bc54dd01d93a12c608ac6937084feca64c9
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2016-01-17 07:09:38 +0000

    Applied Angular Material Design, Passport integration for Google and a dummy Local strategy
    See login page for Material design

--- Core/Angular-1/AngularAuthenticationJS/app.js
+++ Core/Angular-1/AngularAuthenticationJS/app.js
@@ -3,15 +3,38 @@ var http = require('http');
 var path = require('path');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+var passportConfig = require('./config/passportConfig.js');
 
 //==================================================================
 // Define the strategy to be used by PassportJS
 passport.use(new LocalStrategy(
   function(username, password, done) {
-    if (username === "admin" && password === "admin") // stupid example
+    if (username === "kunal.v.mandalia@gmail.com" && password === "helloworld") // stupid example
       return done(null, {name: "admin"});
 
     return done(null, false, { message: 'Incorrect username.' });
+  }
+));
+
+// Google strategy
+passport.use(new GoogleStrategy({
+    clientID: passportConfig["Google"].clientID,
+    clientSecret: passportConfig["Google"].clientSecret,
+    callbackURL: passportConfig["Google"].callbackURL
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      
+      // To keep the example simple, the user's Google profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the Google account with a user record in your database,
+      // and return that user instead.
+      console.log( JSON.stringify(profile));
+      return done(null, profile);
+    });
   }
 ));
 
@@ -51,6 +74,7 @@ app.use(passport.session());    // Add passport initialization
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
@@ -84,6 +108,17 @@ app.post('/logout', function(req, res){
   res.send(200);
 });
 //==================================================================
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: 'https://www.googleapis.com/auth/userinfo.email'}));
+
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/#/admin');
+    res.send(req.user);
+  });
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
